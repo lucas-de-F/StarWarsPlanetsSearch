22358cbb1dd60f0532702b67b453f51c495f4ea4
lucas-de-f <francalucas937@gmail.com>
2022-04-15 17:03:03 -0300

    refactor: filter inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
 import './App.css';
 import { useDispatch } from 'react-redux';
-import { otherFilters, setFilters, setName } from './Redux/FilterSlice'
+import { setName } from './Redux/FilterSlice'
 import { PlanetCards } from './pages/home'
-import { useState } from 'react';
+import FiltersInputs from './Components/Filters';
 
 function App() {
   const dispatch = useDispatch()
-  const [filter, setFilter] = useState<otherFilters>({
-    diameter: 0,
-    orbital_period: 0,
-    climate: ''
-  })
+
   return (
     <div>
       nome: <input type="text" onChange={(e) => dispatch(setName(e.target.value))}></input>
-      <form>
-        <label htmlFor="diameter">
-          Diâmetro: 
-          <input type="number" onChange={(e: any) => setFilter({ ...filter, diameter: e.target.value})}></input>
-        </label>
-
-        <label htmlFor="orbital_period">
-          periodo orbital: 
-          <input type="number" onChange={(e: any) => setFilter({ ...filter, orbital_period: e.target.value })}></input>
-        </label>
-
-        <label htmlFor="climate">
-          clima: 
-          <select onChange={(e: any) => setFilter({ ...filter, climate: e.target.value })}>
-            <option>temperate</option>
-            <option>arid</option>
-            <option>frozen</option>
-            <option>tropical</option>
-            <option>murky</option>
-          </select>
-        </label>
-
-        <button type='button' onClick={() => dispatch(setFilters(filter))}>enviar</button>
-      </form>
+      <FiltersInputs />
       <PlanetCards />
     </div>
   );
---
+++ src/Components/Filters.tsx
@@ -0,0 +1,40 @@
+import { useDispatch } from 'react-redux';
+import { otherFilters, setFilters } from '../Redux/FilterSlice'
+import { useState } from 'react';
+
+function FiltersInputs() {
+  const dispatch = useDispatch()
+  const [filter, setFilter] = useState<otherFilters>({
+    diameter: 0,
+    orbital_period: 0,
+    climate: ''
+  })
+  return (
+      <form>
+        <label htmlFor="diameter">
+          Diâmetro: 
+          <input type="number" onChange={(e: any) => setFilter({ ...filter, diameter: e.target.value})}></input>
+        </label>
+
+        <label htmlFor="orbital_period">
+          periodo orbital: 
+          <input type="number" onChange={(e: any) => setFilter({ ...filter, orbital_period: e.target.value })}></input>
+        </label>
+
+        <label htmlFor="climate">
+          clima: 
+          <select onChange={(e: any) => setFilter({ ...filter, climate: e.target.value })}>
+            <option>temperate</option>
+            <option>arid</option>
+            <option>frozen</option>
+            <option>tropical</option>
+            <option>murky</option>
+          </select>
+        </label>
+
+        <button type='button' onClick={() => dispatch(setFilters(filter))}>enviar</button>
+      </form>
+  );
+}
+
+export default FiltersInputs;
