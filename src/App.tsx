cc4bbde3f05dbb07e8b67ac5374ab8b9b0e8e832
lucas-de-f <francalucas937@gmail.com>
2022-04-15 01:47:17 -0300

    feat: test RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { AnyAction, EnhancedStore, Reducer } from '@reduxjs/toolkit';
 import { RootState } from './Redux/store'
+import { setName, setDiameter, setPopulation } from './Redux/FilterSlice'
 
 function App() {
+  const dispatch = useDispatch();
   const planetFilter = useSelector((state: RootState) => state.planetFilter)
   return (
     <div className="App">
       <header className="App-header" style={{ display: 'flex', flexDirection: 'column'}}>
         <div>
-        nome { planetFilter.name }
+        nome: { planetFilter.name }
         </div>
+
         <div>
-        população { planetFilter.population }
+        <input type="text" id="name" onChange={(e) => dispatch(setName(e.target.value))} />
         </div>
+
+        <div>
+        população: { planetFilter.population }
+        </div>
+
+        <div>
+        <input type="number" id="população" onChange={(e) => dispatch(setDiameter(Number(e.target.value)))} />
+        </div>
+
         <div>
         diâmetro { planetFilter.diameter }
         </div>
+
+        <div>
+        <input type="number" id="diâmetro" onChange={(e) => dispatch(setPopulation(Number(e.target.value)))} />
+        </div>
+
       </header>
     </div>
   );
