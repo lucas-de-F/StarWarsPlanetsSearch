9af1e1e8892427fc143df829502321c090b0a4b8
lucas-de-f <francalucas937@gmail.com>
2022-04-17 20:14:06 -0300

    feat: css main title

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,36 @@ import { Home } from './pages/home'
 
 function App() {
   return (
-    <div className="parent color1">
-      <div className="hero color1">a </div>
-      <div className="title">
-        <h3 className='geek pading0'>geek</h3>
-        <h1 className='websiteLogo pading0'>web site</h1>
+    <body className="parent bodyColor">
+      <div className='div1 darkColor'>
+        
       </div>
-      <div className="nav color2">
+      <header className="title darkColor">
+        <h3 className='geek pading0 color2'>geek</h3>
+        <h1 className='websiteLogo pading0 color2'>web site</h1>
+      </header>
+        
+      <nav className="nav color2 navColor" />
+        <div className="navItems ">
+          <div className="item1 navItemsFont">about </div>
+          <div className="item2 navItemsFont">home </div>
+          <div className="item3 navItemsFont">news </div>
+          <div className="item4 navItemsFont">a </div>
+        </div>
+
+      <div className="main darkColor"></div>
+      <div className="mainTitle pading0">
+        <p className='braking color2'>BREAKING</p>
+        <svg className="newsSVG" width="177" height="72" viewBox="0 0 177 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M28 0H37L42 11.7818H177L158 65H28L9 72H0L28 0Z" fill="#DE2D2D"/>
+          <path d="M43.76 59L57.648 20.6H46.192L41.648 34.296L40.432 20.664L29.04 20.6L14.96 59C21.9573 59 25.456 59 25.456 59L31.216 45.176L31.344 59H43.76Z" fill="white"/>
+          <path d="M70.8405 31.48L69.7525 34.296H84.9845L81.2725 45.112L65.5285 45.176L64.1845 48.632L79.9925 48.568L76.1525 59H54.7125C54.9258 59 53.9658 56.568 51.8325 51.704L61.0485 26.808L68.7925 20.6H89.8485L86.0725 31.416L70.8405 31.48Z" fill="white"/>
+          <path d="M111.725 48.568H115.18L125.549 20.6H137.005L128.044 45.24L116.076 59H104.877C104.791 58.5307 104.557 57.4 104.173 55.608L101.229 59H90.0285C89.5592 56.696 89.0685 54.3707 88.5565 52.024C87.5325 47.3307 86.9992 44.984 86.9565 44.984L95.8525 20.6L107.244 20.664L96.9405 48.568H100.333L110.701 20.6H122.221L111.725 48.568Z" fill="white"/>
+          <path d="M157.114 37.368L148.602 37.304L150.778 31.48L154.938 31.416L153.53 35.192H164.794L167.546 26.872L163.578 20.6H148.73L140.986 26.808L137.722 36.792C140.495 41.4427 141.775 43.768 141.562 43.768H150.458L148.41 48.696H144.058L145.466 45.432H134.266L131.77 51.704C133.903 56.568 134.863 59 134.65 59H149.562L157.754 53.88L161.658 43.512L157.114 37.368Z" fill="white"/>
+        </svg>
       </div>
-      <div className="navItems ">a </div>
-      <div className="item1 ">a </div>
-      <div className="item2 ">a </div>
-      <div className="item3 ">a </div>
-      <div className="item4 ">a </div>
-      <div className="main color3">a </div>
-      <div className="mainTitle ">a </div>
+
+
       <div className="mainText ">a </div>
       <div className="mainSupportText ">a </div>
       <div className="mainFooter ">a </div>
@@ -29,7 +44,7 @@ function App() {
       <div className="sectionFooter color7">a </div>
       <div className="footer color7">a </div>
       <div className="section1 color5">  </div>
-    </div>
+    </body>
   );
 }
 
