2975a4d304d8afd422d78090f89121852e463a8f
lucas-de-f <francalucas937@gmail.com>
2022-04-17 20:57:06 -0300

    feat: component header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,21 @@
+import '../App.css';
+
+export function Header() {
+  return (
+    <>
+    <div className='div1 darkColor'></div>
+    <header className="title darkColor">
+      <h3 className='geek pading0 color2'>geek</h3>
+      <h1 className='websiteLogo pading0 color2'>web site</h1>
+    </header>
+    <nav className="nav color2 navColor" />
+    <div className="navItems ">
+      <div className="item1 navItemsFont">about </div>
+      <div className="item2 navItemsFont">home </div>
+      <div className="item3 navItemsFont">news </div>
+      <div className="item4 navItemsFont">a </div>
+    </div>
+  </>
+
+  )
+}
