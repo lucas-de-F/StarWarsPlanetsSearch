c33218580333f094a39be546a498724cf0fb0b0a
lucas-de-f <francalucas937@gmail.com>
2022-04-24 22:58:45 -0300

    feat: planet card

--- src/Components/cardplanet.tsx
+++ src/Components/cardplanet.tsx
@@ -8,8 +8,16 @@ export const Planets = () => {
         const { name, population, diameter } = filtered[index]
 
         return (
-            <div style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', alignItems: 'center', margin: 100 }}>
-                <div style={{ backgroundColor: '#003566', width: 200, height: 300, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap', alignItems: 'center', margin: 60 }}>
+                <div style={{
+                    backgroundColor: '#003566',
+                    width: 200,
+                    height: 300,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    borderRadius: '40px 40px 40px 40px'    
+                }}>
                 
                 <svg width="144" height="144" style={{ margin: 20 }}  viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="72" cy="72" r="72" style={{ boxShadow: '0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0)'}} fill={ `#${Math.floor(Math.random()*16777215).toString(16)}`}/>
@@ -18,9 +26,9 @@ export const Planets = () => {
                 <p style={{ color: 'white' }}>
                     {index} of {filtered.length}
                 </p>
-                    <Typography style={{ alignSelf: 'center'}}>name: {name}</Typography>
-                    <Typography style={{ alignSelf: 'center'}}>population: {population}</Typography>
-                    <Typography style={{ alignSelf: 'center'}}>diameter: {diameter}</Typography>
+                    <Typography style={{ alignSelf: 'center', fontFamily: 'gotham book', color:'#FFD60A'}}>name: {name}</Typography>
+                    <Typography style={{ alignSelf: 'center', fontFamily: 'gotham book', color:'#FFD60A'}}>population: {population}</Typography>
+                    <Typography style={{ alignSelf: 'center', fontFamily: 'gotham book', color:'#FFD60A', marginBottom: 20}}>diameter: {diameter}</Typography>
                 </div>
             </div>
         )
